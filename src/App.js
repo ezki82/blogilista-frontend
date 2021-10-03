86b8f5131f7f5183590c163274a3dc183bf5410b
Esa Salminen <esa_salminen@hotmail.com>
2021-10-03 21:00:54 +0300

    5.1 tehtävä kesken

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <div>
+      <h2>Blogs</h2>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
+    </div>
+  )
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  return (
+    <div>
+      {loginForm()}
+      {blogForm()}
+    </div>
+  )
+}
+
+export default App
