dd8fe9fc72e82ca1bbb8ca8c04f735aa9f04c904
Esa Salminen <esa_salminen@hotmail.com>
2021-10-11 18:22:23 +0300

    5.6 ja 5.7 tehty, 1h

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  const hideDetailsVisible = {display: detailsVisible ? 'none' : ''}
+  const showDetailsVisible = {display: detailsVisible ? '' : 'none'}
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideDetailsVisible}>
+        <h4>{blog.title} {blog.author}</h4> <button onClick={toggleDetails}>show details</button>          
+        </div>
+        <div style={showDetailsVisible}>
+        <h4>{blog.title} {blog.author}</h4> <button onClick={toggleDetails}>hide details</button> <br/>
+        <p>
+          url:{blog.url} <br/>
+          likes: {blog.likes} <button>like</button> <br/>
+          added by: {blog.user.name}
+        </p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
