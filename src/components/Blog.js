1d9efb88164e66aceccc1e9ca17481ba89c0fc13
Esa Salminen <esa_salminen@hotmail.com>
2021-11-16 20:30:49 +0200

    7.19 ja 7.20 tehty, 1,5h

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,34 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { commentBlog, likeBlog } from '../reducers/blogReducer'
-import styled from 'styled-components'
 
-const Button = styled.button`
-  background-color: whitesmoke;
-  font-weight: bold;
-  padding: 2px;
-  margin-left: 5px;
-`
-const Input = styled.input`
-  background-color: lightgrey;
-  height: 20px;
-  font-size: large;
-`
-const Title = styled.h2`
-
-`
-const Subtitle = styled.h3`
-  font-size: larger;
-`
-
-const Body = styled.div`
-  background-color: darkgrey;
-`
-const ClickLink = styled(Link)`
-  border: 3px;
-  border-color: black;
-  background-color: grey;
-`
 
 const Blog = ({ blog }) => {
   const [comment, setComment] = useState('')
@@ -51,20 +24,20 @@ const Blog = ({ blog }) => {
   }
 
   return (
-    <Body>
-      <Title>{blog.title}</Title>
-      <ClickLink to={blog.url}>{blog.url}</ClickLink>
-      <p>{blog.likes} likes <Button onClick={_addLike}>like</Button><br></br>
+    <div className='form'>
+      <h2>{blog.title}</h2>
+      <Link to={blog.url}>{blog.url}</Link>
+      <p>{blog.likes} likes <button className='button' onClick={_addLike}>like</button><br></br>
       added by {blog.user.name}</p>
-      <Subtitle>comments</Subtitle>
+      <h3>comments</h3>
       <form onSubmit={_addComment}>
-        <Input type='text' value={comment} onChange={({ target }) => setComment(target.value)}/>
-        <Button type='submit'>add comment</Button>
+        <input className='input' type='text' value={comment} onChange={({ target }) => setComment(target.value)}/>
+        <button className='button' type='submit'>add comment</button>
       </form>
       <ul>
         {blog.comments.map((c, index) => <li key={index}>{c}</li>)}
       </ul>
-    </Body>
+    </div>
   )
 }
 
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -5,7 +5,7 @@ import BlogTitle from './BlogTitle'
 const Blogs = () => {
   const blogs = useSelector(state => state.blog)
   return (
-    <div>
+    <div className='form'>
       <h2>Blogs</h2>
       {blogs.map(blog => <BlogTitle key={blog.id} blog={blog}/>)}
     </div>
