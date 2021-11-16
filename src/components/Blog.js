135ef2a201f7877c4d0678bfd308c85ad040bfbe
Esa Salminen <esa_salminen@hotmail.com>
2021-11-16 15:03:27 +0200

    7.18 kesken, 0,5h

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,6 +2,34 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { commentBlog, likeBlog } from '../reducers/blogReducer'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: whitesmoke;
+  font-weight: bold;
+  padding: 2px;
+  margin-left: 5px;
+`
+const Input = styled.input`
+  background-color: lightgrey;
+  height: 20px;
+  font-size: large;
+`
+const Title = styled.h2`
+
+`
+const Subtitle = styled.h3`
+  font-size: larger;
+`
+
+const Body = styled.div`
+  background-color: darkgrey;
+`
+const ClickLink = styled(Link)`
+  border: 3px;
+  border-color: black;
+  background-color: grey;
+`
 
 const Blog = ({ blog }) => {
   const [comment, setComment] = useState('')
@@ -23,20 +51,20 @@ const Blog = ({ blog }) => {
   }
 
   return (
-    <div>
-      <h2>{blog.title}</h2>
-      <Link to={blog.url}>{blog.url}</Link>
-      <p>{blog.likes} likes <button onClick={_addLike}>like</button><br></br>
+    <Body>
+      <Title>{blog.title}</Title>
+      <ClickLink to={blog.url}>{blog.url}</ClickLink>
+      <p>{blog.likes} likes <Button onClick={_addLike}>like</Button><br></br>
       added by {blog.user.name}</p>
-      <h3>comments</h3>
+      <Subtitle>comments</Subtitle>
       <form onSubmit={_addComment}>
-        <input type='text' value={comment} onChange={({ target }) => setComment(target.value)}/>
-        <button type='submit'>add comment</button>
+        <Input type='text' value={comment} onChange={({ target }) => setComment(target.value)}/>
+        <Button type='submit'>add comment</Button>
       </form>
       <ul>
         {blog.comments.map((c, index) => <li key={index}>{c}</li>)}
       </ul>
-    </div>
+    </Body>
   )
 }
 
